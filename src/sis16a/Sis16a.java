2fcf1798d63319749aa5126bf88b241181eab46a
pc <pc@172.16.14.102>
2016-05-27 08:10:48 -0600

    modifique

--- src/sis16a/Sis16a.java
+++ src/sis16a/Sis16a.java
@@ -16,7 +16,7 @@ public class Sis16a {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hola mundo");
     }
     
 }
